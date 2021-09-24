db1892ca8386fe91afd3e1b6559e11489dc9d576
Alan C. Reis <alanc.reis@hotmail.com>
2021-09-23 22:40:46 -0300

    03 - Comportamentos de classes

--- CSharpPOO/ByteBank/ContaCorrente.cs
+++ CSharpPOO/ByteBank/ContaCorrente.cs
@@ -12,5 +12,35 @@ namespace ByteBank
         public int Agencia { get; set; }
         public int Conta { get; set; }
         public double Saldo { get; set; }
+
+        public bool Sacar(double valor)
+        {
+            if(this.Saldo < valor)
+            {
+                return false;
+            }
+            this.Saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if(this.Saldo < valor)
+            {
+                return false;
+            }
+                      
+                this.Saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            
+            
+
+        }
     }
 }
--- CSharpPOO/ByteBank/Program.cs
+++ CSharpPOO/ByteBank/Program.cs
@@ -17,13 +17,32 @@ namespace ByteBank
             Console.Write("Ditege a conta: ");
             conta.Conta = Convert.ToInt32(Console.ReadLine());
 
-            //conta.Saldo = 100.00; //Convert.ToDouble(Console.ReadLine());
+            conta.Saldo = 100.00; //Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(conta.Titular);
             Console.WriteLine(conta.Agencia);
             Console.WriteLine(conta.Conta);
             Console.WriteLine(conta.Saldo);
+
+            Console.Write("Digite o valor do saque: ");
+            conta.Sacar(Convert.ToDouble(Console.ReadLine()));
+            
+            Console.WriteLine("Novo saldo: " + conta.Saldo);
+
+            Console.Write("Digite o valor do depósito: ");
+            conta.Depositar(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Novo saldo: " + conta.Saldo);
+
+            //Criar conta somente para realizar o metodo transferir
+            ContaCorrente c2 = new ContaCorrente();
+            
+            Console.Write("Digite o valor da tranferência: ");
+            conta.Transferir(Convert.ToDouble(Console.ReadLine()), c2);
+
+            Console.WriteLine("Novo saldo: " + conta.Saldo);
             Console.ReadLine();
+
         }
     }
 }
