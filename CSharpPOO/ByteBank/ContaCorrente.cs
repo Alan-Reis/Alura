e95588beaa6bfddb73cd3ec137a9f4505bf65ac6
Alan C. Reis <alanc.reis@hotmail.com>
2021-09-23 21:35:45 -0300

    01 - Nossa primeira classe

---
+++ CSharpPOO/ByteBank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public string Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
---
+++ CSharpPOO/ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            Console.Write("Digite o nome do titular: ");
+            conta.Titular = Console.ReadLine();
+
+            Console.Write("Digite a agência: ");
+            conta.Agencia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ditege a conta: ");
+            conta.Conta = Convert.ToInt32(Console.ReadLine());
+
+            //conta.Saldo = 100.00; //Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(conta.Titular);
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Conta);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
